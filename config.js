1dca756b6fcd44624019ad3f7937e9daed961788
troudufion <mohavsk1@gmail.com>
2025-02-14 23:34:19 +0100

    config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
         prefixe: '+', // Préfixe - Le texte avant les commandes
         buyer: '1216737507912323156', // Buyer
         couleur: '#6495ED', // Couleur HEX
-        footer: '[+]', // Dans la commande help ou autre il y'a un texte en bas
+        footer: 'main', // Dans la commande help ou autre il y'a un texte en bas
         maxServer: '100', // Le nombre de serveur maximum que le bot peut rejoindre 
     }
 }
