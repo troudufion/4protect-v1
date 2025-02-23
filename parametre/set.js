0953b90d0b0b84d070a6f2d68e427f99f20327af
troudufion <mohavsk1@gmail.com>
2025-02-23 02:08:19 +0100

    set.js

--- parametre/set.js
+++ parametre/set.js
@@ -40,13 +40,13 @@ module.exports = {
 
                     case 'banner':
                         if (!client.user.banner) {
-                            return message.reply(`❌ **Impossible de changer la bannière** : seuls les **bots vérifiés et boostés avec Nitro** peuvent avoir une bannière.`);
+                            return message.reply(` Impossible de changer la bannière** : seuls les **bots vérifiés et boostés avec Nitro peuvent avoir une bannière.`);
                         }
                         if (message.attachments.size > 0) {
                             const attachment = message.attachments.first();
                             client.user.setBanner(attachment.url)
                                 .then(() => message.channel.send(` **Bannière du bot mise à jour !**`))
-                                .catch(() => message.reply(`⚠️ Une erreur s'est produite, veuillez vérifier que l'image est bien valide.`));
+                                .catch(() => message.reply(`⚠ Une erreur s'est produite, veuillez vérifier que l'image est bien valide.`));
                         } else {
                             client.user.setBanner(content)
                                 .then(() => message.channel.send(` **Bannière du bot mise à jour !**`))
@@ -59,10 +59,10 @@ module.exports = {
                         break;
                 }
             } else {
-                message.reply(`❌ Veuillez fournir une option (**name, pic, banner**) suivie du contenu (**nom ou lien**).`);
+                message.reply(` Veuillez fournir une option (**name, pic, banner**) suivie du contenu (**nom ou lien**).`);
             }
         } else {
-            message.reply(`❌ **Vous n'avez pas la permission d'utiliser cette commande !**`);
+            message.reply(` Vous n'avez pas la permission d'utiliser cette commande !`);
         }
     }
 };
